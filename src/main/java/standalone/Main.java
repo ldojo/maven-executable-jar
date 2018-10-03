86246d1e9e5993fb0488666abdc29797ff25c37b
Lev Shulman <lev.shulman@levvel.io>
2018-10-02 21:52:27 -0600

    some gitignores

--- src/main/java/standalone/Main.java
+++ src/main/java/standalone/Main.java
@@ -3,7 +3,7 @@ package standalone;
 public class Main {
   
   public static void main(String[] args) {
-    System.out.println("main running");
+    System.out.println("executable running!");
   }
 
 }
